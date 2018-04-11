01cffcc0e0ea3f35d796ed2cdd32d55ad5354e86
hjmh <hjmhaberkern@gmail.com>
2018-04-11 14:43:27 -0400

    added function to import invisible objects

--- utilities/loadObjectCoords.py
+++ utilities/loadObjectCoords.py
@@ -43,6 +43,7 @@ def loadObjectCoordIdentities(dataDir, coordFile):
         lines = Coof.read().splitlines()
 
     invisibleObjectCoords = []
+    invisibleObjectName = []
     visibleObjectCoords = []
     visibleObjectName = []
     origin = []
@@ -56,6 +57,7 @@ def loadObjectCoordIdentities(dataDir, coordFile):
         if currObject[0][0] == '_':
             if currObject[0] not in ['_start_', '_camera_block_pm_', '_cylinder_pic_', '_Plane_p_']:
                 invisibleObjectCoords.append(np.asarray((float(currObject[2]), float(currObject[3]))))
+                invisibleObjectName.append(currObject[0])
 
         elif currObject[0] == 'Origin':
             origin = [float(currObject[2]), float(currObject[3])]
@@ -67,3 +69,36 @@ def loadObjectCoordIdentities(dataDir, coordFile):
     visibleObjectCoords = np.asarray(visibleObjectCoords)
 
     return visibleObjectCoords, visibleObjectName, invisibleObjectCoords, origin
+
+def loadObjectCoordAllIdentities(dataDir, coordFile):
+    # Read coord file and extract coordinates of visible objects
+    with open(dataDir + 'rawData/' + coordFile) as Coof:
+        lines = Coof.read().splitlines()
+    
+    invisibleObjectCoords = []
+    invisibleObjectName = []
+    visibleObjectCoords = []
+    visibleObjectName = []
+    origin = []
+
+    for line in range(len(lines)):
+        currObject = lines[line].split(' ')
+        
+        if len(currObject) < 3:
+            continue
+        
+        if currObject[0][0] == '_':
+            if currObject[0] not in ['_start_', '_camera_block_pm_', '_cylinder_pic_', '_Plane_p_']:
+                invisibleObjectCoords.append(np.asarray((float(currObject[2]), float(currObject[3]))))
+                invisibleObjectName.append(currObject[0])
+    
+        elif currObject[0] == 'Origin':
+            origin = [float(currObject[2]), float(currObject[3])]
+        else:
+            visibleObjectCoords.append(np.asarray((float(currObject[2]), float(currObject[3]))))
+            visibleObjectName.append(currObject[0])
+
+    invisibleObjectCoords = np.asarray(invisibleObjectCoords)
+    visibleObjectCoords = np.asarray(visibleObjectCoords)
+
+    return visibleObjectCoords, visibleObjectName, invisibleObjectCoords, invisibleObjectName, origin
